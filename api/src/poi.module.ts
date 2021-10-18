709a631fc255baad32c117063618df38f4161509
L-charleux <loiccharleux71@gmail.com>
2021-10-18 15:06:40 +0200

    api v2

--- api/src/poi.module.ts
+++ api/src/poi.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PoIController } from './poi.controller';
 import { PoIService } from './poi.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [PoIController],
   providers: [PoIService],
 })
