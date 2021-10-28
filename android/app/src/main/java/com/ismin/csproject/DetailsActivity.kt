bc125f1e6887a560640b2b66f9e3ccbecdd34937
L-charleux <loiccharleux71@gmail.com>
2021-10-28 15:04:28 +0200

    android v3

--- android/app/src/main/java/com/ismin/csproject/DetailsActivity.kt
+++ android/app/src/main/java/com/ismin/csproject/DetailsActivity.kt
@@ -2,10 +2,14 @@ package com.ismin.csproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.bumptech.glide.Glide
+//import com.bumptech.glide.Glide
+import com.nostra13.universalimageloader.core.DisplayImageOptions
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -32,11 +36,26 @@ class DetailsActivity : AppCompatActivity() {
         txvType.text = "Type: " + poiFromMain.type + poiFromMain.level;
         txvCommentary.text = "Commentary: " + poiFromMain.commentary;
 
+        /*
         Glide.with(this)
             .load(poiFromMain.pictureLink)
             .placeholder(R.drawable.test)
             .error(R.drawable.ic_favorite)
             .fallback(R.drawable.ic_golden_gym)
-            .into(imvImage)
+            .into(imvImage)*/
+
+        val imageLoader = ImageLoader.getInstance()
+        imageLoader.init(ImageLoaderConfiguration.createDefault(applicationContext))
+
+        val imageUri = poiFromMain.pictureLink
+
+        val options =
+            DisplayImageOptions.Builder() // stub image will display when your image is loading
+                .showStubImage(R.drawable.test) // below image will be displayed when the image url is empty
+                .showImageForEmptyUri(R.drawable.ic_favorite) // cachememory method will caches the image in users external storage
+                .build()
+        // below method will display image inside our image view..
+        // below method will display image inside our image view..
+        imageLoader.displayImage(imageUri, imvImage, options, null)
     }
 }
